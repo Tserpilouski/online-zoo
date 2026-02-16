f2ff087a98439c26058aa7bab4c97e2174e99a38
tserpilouski <terpilovskikirill@gmail.com>
2026-02-16 09:58:52 +0100

    feat: add initial project structure

---
+++ online-zoo/vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+    root: ".",
+    build: {
+        outDir: "dist",
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                landing: resolve(__dirname, "pages/landing/index.html"),
+                map: resolve(__dirname, "pages/map/index.html"),
+                zoos: resolve(__dirname, "pages/zoos/index.html"),
+                "contact-us": resolve(
+                    __dirname,
+                    "pages/contact-us/index.html",
+                ),
+            },
+        },
+        cssCodeSplit: false,
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                api: "modern-compiler",
+            },
+        },
+    },
+});
