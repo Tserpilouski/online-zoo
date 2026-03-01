71ab18c18b13072f6cf5aa68735760b5d2b7bb7a
Tserpilouski <terpilovskikirill@gmail.com>
2026-03-01 23:51:52 +0100

    fix: fix zoo page

--- online-zoo/pages/contact-us/script.js
+++ online-zoo/pages/contact-us/script.js
@@ -1,1 +1,16 @@
 import './style.scss';
+
+const burgerBtn = document.getElementById('header-burger');
+const mobileMenu = document.getElementById('header-mobile-menu');
+const mobileClose = document.getElementById('header-mobile-close');
+
+if (burgerBtn && mobileMenu && mobileClose) {
+	burgerBtn.addEventListener('click', () => {
+		mobileMenu.classList.add('header__mobile-menu--open');
+		document.body.style.overflow = 'hidden';
+	});
+	mobileClose.addEventListener('click', () => {
+		mobileMenu.classList.remove('header__mobile-menu--open');
+		document.body.style.overflow = '';
+	});
+}
